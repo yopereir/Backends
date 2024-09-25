0de80c0fcef1a3ab5c4c6fb842395924cf554f78
Yohann Pereira <ypereira@bgea.org>
2024-09-25 09:31:05 -0400

    added more tech stacks

--- prometheus/deploy.sh
+++ prometheus/deploy.sh
@@ -1,4 +1,12 @@
 export NAMESPACE="prometheus"
+
+# Set dns resolution
+if $(cat /etc/hosts | grep -q prometheus.test);then
+ sudo sed -i -e "s/.*prometheus.test.*/$(echo "127.0.0.1 prometheus.test")/" /etc/hosts;
+ else 
+ echo "127.0.0.1 prometheus.test" | sudo tee -a /etc/hosts;
+fi
+
 kubectl delete namespace $NAMESPACE || echo 0
 kubectl create namespace $NAMESPACE
 kubectl apply -f . --namespace $NAMESPACE
