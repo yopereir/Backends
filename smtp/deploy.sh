268ed1d7c10ced98903eedab56d20ea5e86b0902
Yohann Pereira <ypereira@bgea.org>
2025-01-03 15:06:10 -0500

    moved smtp and webapi to Middles and Frontends respectively

--- smtp/deploy.sh
+++
@@ -1,5 +0,0 @@
-export NAMESPACE="smtp"
-docker build -t smtp .
-kubectl delete namespace $NAMESPACE || echo 0
-kubectl create namespace $NAMESPACE
-kubectl apply -R -f ./*.yaml --namespace $NAMESPACE
