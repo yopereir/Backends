af5592b7eee71c3b2694f55f544290eb88263f00
Yohann Pereira <ypereira@bgea.org>
2025-05-28 16:05:56 -0400

    updated kafka stack

--- kafka/deploy.sh
+++ kafka/deploy.sh
@@ -2,4 +2,11 @@ export NAMESPACE="kafka"
 
 kubectl delete namespace $NAMESPACE || echo 0
 kubectl create namespace $NAMESPACE
-kubectl apply -R -f ./generated --namespace $NAMESPACE
+# DEPLOY HELM CHART- BITNAMI
+# kubectl apply -R -f ./stacks/helm/bitnami/templates/ --namespace $NAMESPACE
+
+# DEPLOY Kubernetes simple with GUI
+kubectl apply -R -f ./stacks/k8s/simple/ --namespace $NAMESPACE
+
+# View GUI from browser
+kubectl port-forward --namespace $NAMESPACE svc/kafka-ui 8080:80
