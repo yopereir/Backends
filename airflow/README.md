2fc88a1466b97bbeb1842ebb4c1c206df51f23b0
Yohann Pereira <ypereira@bgea.org>
2025-06-02 12:40:28 -0400

    updated airflow stack

--- airflow/README.md
+++ airflow/README.md
@@ -5,6 +5,7 @@ A server for managing crons and data/event driven pipelines.
 * This deployment makes use of the official Helm chart.
 * Run `./deploy.sh` to deploy stack on connectect kubernetes cluster.
 * To re-generate kubernetes object files run the commented out code in `deploy.sh`.
+* Default webserver creds- admin, admin.
 
 ## Conventions
 * `scripts` folder contains general scripts to interact with Airflow.
--- airflow/deploy.sh
+++ airflow/deploy.sh
@@ -7,4 +7,8 @@ kubectl create namespace $NAMESPACE
 # helm repo add apache-airflow https://airflow.apache.org
 # helm repo update
 # helm template --output-dir ./generated apache-airflow/airflow --set auth.rbac.create=false,readinessProbe.enabled=false,livenessProbe.enabled=false,startupProbe.enabled=false,podSecurityContext.enabled=false,containerSecurityContext.enabled=false,fullnameOverride=airflow --name-template airflow --set webserverSecretKey=c24e0aed99965cf0c4cbe66a8d1d79d3 --namespace $NAMESPACE
+docker build -t airflow-test .
 kubectl apply -R -f ./generated/ --namespace $NAMESPACE
+
+# Port Forward
+# kubectl port-forward --namespace $NAMESPACE svc/airflow-webserver 8080:8080
