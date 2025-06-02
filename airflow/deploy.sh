8ecf824117179c4697b7880880c8e45688b92dbe
Yohann Pereira <ypereira@bgea.org>
2025-06-02 13:37:42 -0400

    updated airflow stack

--- airflow/deploy.sh
+++ airflow/deploy.sh
@@ -6,8 +6,8 @@ kubectl create namespace $NAMESPACE
 # rm -rf generated
 # helm repo add apache-airflow https://airflow.apache.org
 # helm repo update
-# helm template --output-dir ./generated apache-airflow/airflow --set auth.rbac.create=false,readinessProbe.enabled=false,livenessProbe.enabled=false,startupProbe.enabled=false,podSecurityContext.enabled=false,containerSecurityContext.enabled=false,fullnameOverride=airflow --name-template airflow --set webserverSecretKey=c24e0aed99965cf0c4cbe66a8d1d79d3 --namespace $NAMESPACE
-docker build -t airflow-test .
+# helm template --output-dir ./generated apache-airflow/airflow --set auth.rbac.create=false,readinessProbe.enabled=false,livenessProbe.enabled=false,startupProbe.enabled=false,podSecurityContext.enabled=false,containerSecurityContext.enabled=false,fullnameOverride=airflow --name-template airflow --set webserverSecretKey=c24e0aed99965cf0c4cbe66a8d1d79d3,images.airflow.repository=custom-airflow,images.airflow.tag=test,images.airflow.pullPolicy=Never --namespace $NAMESPACE
+docker build -t custom-airflow:test .
 kubectl apply -R -f ./generated/ --namespace $NAMESPACE
 
 # Port Forward
