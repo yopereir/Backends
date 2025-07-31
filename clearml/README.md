8d7cf273f697e9ac14040b1b68d2d28c624ac87c
Yohann Pereira <ypereira@bgea.org>
2025-07-31 10:51:25 -0400

    added ClearML

---
+++ clearml/README.md
@@ -0,0 +1,14 @@
+# ClearML
+OLAP data analytics framework.
+
+# How Tos
+* Connect to deployed clearml server
+    - Install clearml library for python `python -m pip install clearml`.
+    - Make sure clearml-apiserver and clearml-webserver are running at localhost:8008 and localhost:8080 respectively.
+    - Go here(http://localhost:8080/settings/workspace-configuration) to get credentials then type `clearml-init` in terminal.
+    - Then run `python ./scripts/tests/createTask.py`.
+
+## Conventions
+- `stacks` folder contain different implementations of the full clearml stack.
+- Within the `stacks` folder, `Dockerfile` contains the base clearml image, `./secrets/.env.ENVIRONMENT` contains the secrets used by clearml and all `.yaml` files define the kubernetes stack.
+- `deploy.sh` script deploys the stack. Use `./deploy.sh -fn` to delete all previous resources for a clean setup. Use `./deploy.sh -i` to setup an ingress tunnel on port 80.
---
+++ clearml/deploy.sh
@@ -0,0 +1,53 @@
+export NAMESPACE="clearml"
+export ENVIRONMENT="loc"
+export DYNAMIC_IMAGE_NAME=$NAMESPACE"-$ENVIRONMENT"
+export STACK="simple"
+
+while test $# -gt 0; do
+  case "$1" in
+    -i|--ingress)
+      echo "Ingress controller will be installed and port-forwarded to 80."
+      kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.2/deploy/static/provider/cloud/deploy.yaml
+      sleep 20
+      kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+      sudo kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 80:80
+      exit 0
+      shift
+      ;;
+    -fn|--fresh-namespace)
+      echo "Namespace will be deleted and re-created."
+      kubectl delete namespace $NAMESPACE || echo 0
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+# Set dns resolution
+if $(cat /etc/hosts | grep -q clearml.test);then
+ sudo sed -i -e "s/.*clearml.test.*/$(echo "127.0.0.1 clearml.test")/" /etc/hosts;
+ else 
+ echo "127.0.0.1 clearml.test" | sudo tee -a /etc/hosts;
+fi
+
+# Build docker image
+docker build -t $DYNAMIC_IMAGE_NAME -f ./stacks/k8s/$STACK/Dockerfile .
+
+# Create and deploy k8s resources
+kubectl create namespace $NAMESPACE
+
+
+# Deploy helm chart
+## Install Helm chart
+# helm repo add clearml https://clearml.github.io/clearml-helm-charts
+## Generate k8s objects from helm chart
+rm -rf ./stacks/helm/generated/*
+helm template clearml/clearml --output-dir ./stacks/helm/generated/ -f ./stacks/helm/values.yaml --set fullnameOverride=clearml --namespace $NAMESPACE
+## Deploy k8s objects
+kubectl apply -R -f ./stacks/helm/generated/* --namespace $NAMESPACE
+
+# View GUI from browser
+kubectl port-forward svc/clearml-webserver 8080:80 --namespace $NAMESPACE
+kubectl port-forward svc/clearml-apiserver 8008:80 --namespace $NAMESPACE
