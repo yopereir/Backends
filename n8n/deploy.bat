a781c382d2f9b10bec24a6dc5b4f4326ef4f04d2
yopereir <yohann.pereira28@gmail.com>
2026-01-30 16:43:36 -0500

    updated n8n stack

--- n8n/deploy.bat
+++ n8n/deploy.bat
@@ -1,5 +1,7 @@
 kubectl create namespace n8n
-kubectl create secret generic n8n-secret-env --from-env-file=./stacks/vanilla/.env --namespace n8n
-kubectl apply -f ./stacks/k8s/n8n.yaml -n n8n
-kubectl apply -f ./stacks/k8s/postgres.yaml -n n8n
-kubectl apply -f ./stacks/ai/stack.yaml -n n8n
+kubectl create secret generic n8n-secret --from-env-file=./secrets/.env --namespace n8n
+@REM kubectl apply -f ./stacks/k8s/n8n.yaml -n n8n
+@REM kubectl apply -f ./stacks/k8s/postgres.yaml -n n8n
+@REM OR
+kubectl apply -f ./stacks/k8s/n8n-externalDB.yaml -n n8n
+@REM kubectl apply -f ./stacks/ai/stack.yaml -n n8n
